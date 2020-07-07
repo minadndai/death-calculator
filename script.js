525d29b43ff1d11c240dfecc01c32ca7ef6f8b8e
Mina <65378477+minadndai@users.noreply.github.com>
2020-07-07 16:49:39 +1000

    Added comments on html and js files

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Creating a function to allow user to input their age, and then calculate a result based on random number
+
 function deathCalculator(){
   let age = prompt("Enter your age:");
     let number = (Math.random()*10) + 1;
@@ -10,9 +12,13 @@ function deathCalculator(){
       death = Math.round((age/number) + 30);
     }
 
+// Find #display and then create text as below
+
 document.getElementById("display").innerHTML = "You have " + death + " years to live! Good luck!";
 
 }
+
+// Creating a function to clear #display
 
 function restart(){
   document.getElementById("display").innerHTML = "";
